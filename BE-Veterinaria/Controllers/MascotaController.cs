52eae50e806d415caece92de82a630a3c409cc7b
Ramiro Quesada <ramiroquesadaperez@gmail.com>
2023-07-11 18:12:10 -0300

    Agregar archivos de proyecto.

---
+++ BE-Veterinaria/Controllers/MascotaController.cs
@@ -0,0 +1,101 @@
+﻿using BE_Veterinaria.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Net.WebSockets;
+
+namespace BE_Veterinaria.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MascotaController : ControllerBase
+    {
+        private readonly AplicationDbContext _context;
+
+        public MascotaController(AplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var listMascotas = await _context.Mascotas.ToListAsync();
+                return Ok(listMascotas);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+
+        public async Task<IActionResult> Get(int id)
+        {
+            try
+            {
+                var mascota = await _context.Mascotas.FindAsync(id);
+                if (mascota == null)
+                {
+                    return NotFound();
+                }
+                return Ok(mascota);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                var mascota = await _context.Mascotas.FindAsync(id);
+
+                if (mascota == null)
+                {
+                    return NotFound();
+                }
+
+                _context.Mascotas.Remove(mascota);
+                await _context.SaveChangesAsync();
+
+                return NoContent();
+
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        [HttpPost]
+
+        public async Task<IActionResult> Post(Mascota mascota)
+        {
+            try
+            {
+                mascota.FechaCreacion = DateTime.Now;
+                _context.Add(mascota);
+                await _context.SaveChangesAsync();
+                return CreatedAtAction("Get", new { id = mascota.Id }, mascota);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+
+        }
+    }
+
+}
---
+++ BE-Veterinaria/Models/Mascota.cs
@@ -0,0 +1,15 @@
+﻿namespace BE_Veterinaria.Models
+{
+    public class Mascota
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Raza { get; set; }
+        public string Color { get; set; }
+        public int Edad { get; set; }
+        public float Peso { get; set; }
+        public string Duenio { get; set; }
+        public string Tipo { get; set;}
+        public DateTime FechaCreacion { get; set; }
+    }
+}
